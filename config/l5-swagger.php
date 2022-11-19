3b46aa92e3fc16012c7275566ab3ebbaa2e7daf3
Johan Guse <johanguse@gmail.com>
2022-11-19 14:40:47 -0300

    Update swagger api-docs json path

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -67,7 +67,7 @@ return [
             /*
              * Absolute path to location where parsed annotations will be stored
              */
-            'docs' => storage_path('api-docs'),
+            'docs' => storage_path('storage/api-docs'),
             /*
              * Absolute path to directory where to export views
              */
